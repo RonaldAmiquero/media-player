df765c5190361fdc17f07aab6f9673ac33e8932b
Ronald Amiquero <roamz.2017@gmail.com>
2021-08-15 21:01:17 -0500

    start project

---
+++ assets/plugins/AutoPlay.js
@@ -0,0 +1,8 @@
+function AutoPlay() {}
+AutoPlay.prototype.run = function (player) {
+  if (!player.muted) {
+    player.muted = true;
+  }
+  player.play();
+};
+export default AutoPlay;
---
+++ ejercicios/typescript/index.ts
@@ -0,0 +1,16 @@
+let people: string[] = [];
+people = ['ronald', 'jeral', 'fabi'];
+people.push('javier');
+
+let peopleAndNumbers: Array<string | number> = [];
+peopleAndNumbers.push('Ronald');
+peopleAndNumbers.push(2);
+
+enum Color {
+  Rojo,
+  Verde,
+  Azul,
+}
+
+let colorFavorito: Color = Color.Rojo;
+console.log(colorFavorito);
